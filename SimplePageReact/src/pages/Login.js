ce8248dc3c6dba684b8fab76c146a3947d5df3e0
Diego David <diegodavidq@outlook.com>
2020-04-17 14:58:59 -0500

    React-router-dom antes de usar nodejs

---
+++ SimplePageReact/src/pages/Login.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+import Title from '../components/Title';
+import Container from '../components/Container';
+
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+import {
+    BrowserRouter as ReactRouter,
+    Link,
+    Route
+} from 'react-router-dom';
+
+export default class Login extends React.Component {
+    
+    
+    render() {
+        return (
+                <div className="row middle-xs">
+                    <div className="col-xs-12 col-sm-6">
+                        <Container>
+                            <div style={{ 'textAlign': 'left' }}>
+                                <Title />
+                                <TextField
+                                    floatingLabelText="Correo electrónico"
+                                    type="email"
+                                    className="textfield"
+                                />
+                                <TextField
+                                    floatingLabelText="Contraseña"
+                                    type="password"
+                                    className="textfield"
+                                />
+                                <div className="Login-actions">
+                                    <Route path="/login" exact render={() => {
+                                        return (
+                                            <div>
+                                                <Link to="/signup" style={{ marginRight: "1em" }}>Crear nueva cuenta</Link>
+                                                <RaisedButton label="Ingresar" secondary={true} />
+                                            </div>
+                                        );
+                                    }}></Route>
+
+                                    <Route path="/signup" exact render={() => {
+                                        return (
+                                            <div>
+                                                <Link to="/login" style={{ marginRight: "1em" }}>Ya tengo cuenta</Link>
+                                                <RaisedButton label="Crear cuenta" secondary={true} />
+                                            </div>
+                                        );
+                                    }}></Route>
+                                    <Link to="/" style={{ marginRight: "1em" }}>Inicio</Link>
+                                </div>
+                            </div>
+                        </Container>
+                    </div>
+                    <div className="col-xs-12 col-sm-6">
+                        <div>
+                            <Route path="/login" exact render={() =>
+                                <div className="Login-background" style={{
+                                    'backgroundImage':
+                                        "url(" + process.env.PUBLIC_URL + "/images/Login-background.jpg" + ")"
+                                }}></div>
+                            }></Route>
+
+                            <Route path="/signup" exact render={() => 
+                                <div className="Login-background" style={{
+                                    'backgroundImage':
+                                        "url(" + process.env.PUBLIC_URL + "/images/compartir.jpg" + ")"
+                                }}></div>
+                            }></Route>
+
+                        </div>
+                    </div>
+                </div>
+        )
+    }
+} 
---
+++ SimplePageReact/src/requests/places.js
@@ -0,0 +1,22 @@
+export default {
+  places: [
+    {
+      imageUrl: '/images/place-3.jpg',
+      title: 'Desayunos el rey',
+      description: 'Starbucks Corporation is an American coffee company and coffeehouse chain.',
+      address:'Av Principal #20'
+    },
+    {
+      imageUrl: '/images/place-1.jpg',
+      title: 'Starbucks Norte',
+      description: 'Starbucks Corporation is an American coffee company and coffeehouse chain.',
+      address:'Av Principal #20'
+    },
+    {
+      imageUrl: '/images/place-2.jpg',
+      title: 'Pizza de amor',
+      description: 'Starbucks Corporation is an American coffee company and coffeehouse chain.',
+      address:'Av Principal #20'
+    }
+  ]
+}
